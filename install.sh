7d896ec3739800c3525510ab1aea6729a1c3da88
PansuriaShrey <pansuriashrey@gmail.com>
2025-08-10 22:59:48 +0530

    docs: add explanatory comments to installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Install dependencies
 poetry install
 
+# Build the package
 poetry build
 
-pip install --editable .
+# Install the package in the user's home directory
+python3 -m pip install --user --force-reinstall dist/*.whl
